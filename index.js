b0c694044359a66f78c3e140bfbf686e8370b60f
Dave Amit <self@daveamit.com>
2017-11-08 03:38:56 +0530

    Major changes in api

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const amqp = require('amqplib');
 const server = require('./lib/server');
 const client = require('./lib/client');
 
-module.exports = {
-    init,
-}
-
 async function init(opts) {
-    try {
-        const conn = await amqp.connect(opts);
-        const [on, call] = await Promise.all([server(conn), client(conn)]);
-        module.exports.on = on;
-        module.exports.call = call;
-        return module.exports;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const conn = await amqp.connect(opts);
+    const ch = await conn.createChannel();
+    
+    const [on, call] = await Promise.all([server(ch), client(ch)]);
+    module.exports.on = on;
+    module.exports.call = call;
+    return module.exports;
+  } catch (error) {
+    throw error;
+  }
 }
+module.exports = {
+  init,
+};
