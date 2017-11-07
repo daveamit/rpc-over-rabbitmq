e9266abd341b042b95016acf981acad3465a8791
Dave Amit <self@daveamit.com>
2017-11-07 05:51:17 +0530

    unified implementation, single init resolves into call and on, after the initial init, all modules can directly *require* the module and use call / on as and when require

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+const amqp = require('amqplib');
+
+const server = require('./lib/server');
+const client = require('./lib/client');
+
 module.exports = {
-    server: require('./lib/server'),
-    client: require('./lib/client')
-};
+    init,
+}
+
+async function init(opts) {
+    try {
+        const conn = await amqp.connect(opts);
+        const [on, call] = await Promise.all([server(conn), client(conn)]);
+        module.exports.on = on;
+        module.exports.call = call;
+        return module.exports;
+    } catch (error) {
+        console.log(error);
+    }
+}
