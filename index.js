b5fa329862a102c59552d460b3e5fb53f362f6dd
Dave Amit <self@daveamit.com>
2017-11-08 23:40:45 +0530

    Added support for broadcast and listen

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const amqp = require('amqplib');
 
 const server = require('./lib/server');
 const client = require('./lib/client');
+const broadcastMod = require('./lib/broadcast');
+const newsOnMod = require('./lib/newsOn');
 
 async function init(opts) {
   try {
     const conn = await amqp.connect(opts);
     const ch = await conn.createChannel();
-    
-    const [on, call] = await Promise.all([server(ch), client(ch)]);
+
+    const [on, call, broadcast, newsOn] = await Promise.all([server(ch), client(ch), broadcastMod(ch), newsOnMod(ch)]);
     module.exports.on = on;
     module.exports.call = call;
+    module.exports.broadcast = broadcast;
+    module.exports.newsOn = newsOn;
     return module.exports;
   } catch (error) {
     throw error;
